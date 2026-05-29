320418556fc5ecc55ea3a4053eb8ca2a3935c3a7
bauyrzhan-turganaliyev <bauyrzhan.turganaliyev@gmail.com>
2026-05-29 20:32:10 +0200

    REFACTOR: Made Product fields final to ensure immutability

--- src/main/java/it/unifi/balpha/store/Product.java
+++ src/main/java/it/unifi/balpha/store/Product.java
@@ -1,8 +1,8 @@
 package it.unifi.balpha.store;
 
 public class Product {
-    private String name;
-    private double price;
+    private final String name;
+    private final double price;
 
     public Product(String name, double price) {
         this.name = name;
