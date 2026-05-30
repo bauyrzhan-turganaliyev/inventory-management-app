5645e509a3468b786193e35e667afc0d7343fc92
bauyrzhan-turganaliyev <bauyrzhan.turganaliyev@gmail.com>
2026-05-30 19:20:33 +0200

    RED: Added test for InventoryPresenterImpl interaction with InventoryService

---
+++ src/test/java/it/unifi/balpha/store/InventoryPresenterTest.java
@@ -0,0 +1,28 @@
+package it.unifi.balpha.store;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.mockito.Mockito.verify;
+
+class InventoryPresenterTest {
+
+    private InventoryService inventoryService;
+    private InventoryPresenterImpl presenter;
+
+    @BeforeEach
+    void setUp() {
+        inventoryService = Mockito.mock(InventoryService.class);
+        
+        presenter = new InventoryPresenterImpl(inventoryService);
+    }
+
+    @Test
+    void testAddProductShouldCallServiceToSave() {
+        presenter.addProduct("Apple", 1.50);
+
+        Product expectedProduct = new Product("Apple", 1.50);
+        verify(inventoryService).saveProduct(expectedProduct);
+    }
+}
